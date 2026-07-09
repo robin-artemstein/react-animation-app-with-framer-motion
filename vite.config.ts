9179b6254afbad4c199c73d655b4e5985861666a
RobinArtemstein <otu168x@hotmail.com>
2026-07-09 19:15:09 +0800

    The second commit on 9th July 2026.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"react-animation-app-with-framer-motion",
   plugins: [tailwindcss(),react()],
 })
